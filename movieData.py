f1ee384d4febf362488761ad9e7295ba325b5e3f
Mjcv <max_83_14@hotmail.com>
2024-09-27 12:32:12 -0500

    second commit

---
+++ movieData.py
@@ -0,0 +1,413 @@
+# Released between 2020-01-01 and 2023-12-31
+
+
+{
+    "page": 1,
+    "results": [
+        {
+            "adult": false,
+            "backdrop_path": "/2wrdcWrAN53Pl7ahBWMIKMzXBFF.jpg",
+            "genre_ids": [
+                16,
+                28,
+                12,
+                14,
+                878
+            ],
+            "id": 1134424,
+            "original_language": "th",
+            "original_title": "นักรบมนตรา: ตำนานแปดดวงจันทร์",
+            "overview": "A story inspired by the original RAMAYANA, retold in a futuristic universe, involving brave warriors who possess ancient powers from another dimension.",
+            "popularity": 435.916,
+            "poster_path": "/sQE0VegwDa0GHjawfNRr1sFgVE2.jpg",
+            "release_date": "2023-10-11",
+            "title": "Mantra Warrior: The Legend of The Eight Moons",
+            "video": false,
+            "vote_average": 6.523,
+            "vote_count": 22
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/AjykZQ9ncgyJoHpMTquDRUAv5VO.jpg",
+            "genre_ids": [
+                18
+            ],
+            "id": 592695,
+            "original_language": "sv",
+            "original_title": "Pleasure",
+            "overview": "19 year old Linnéa leaves her small town in Sweden and heads for Los Angeles with the aim of becoming the world's next big porn star, but the road to her goal turns out to be bumpier than she imagined.",
+            "popularity": 365.117,
+            "poster_path": "/7Z2K08J0WantJHNa0vLTOmii41l.jpg",
+            "release_date": "2021-10-08",
+            "title": "Pleasure",
+            "video": false,
+            "vote_average": 6.227,
+            "vote_count": 514
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/wQ60FXOHnVimJo6Bpdk6VU0qaP6.jpg",
+            "genre_ids": [
+                12,
+                10751
+            ],
+            "id": 966576,
+            "original_language": "en",
+            "original_title": "The Quest for Tom Sawyer's Gold",
+            "overview": "Agatha Armstrong and her trusty sidekick Mrs. Mac were once the world’s most daring adventurers. But when her son Ant came along, everything quickly changed and her devil-may-care ways took a backseat to being over-protective and terrified of anything that posed a risk to her family.",
+            "popularity": 353.267,
+            "poster_path": "/zceLrHeYOgJHw1tw6MUi15N2NUz.jpg",
+            "release_date": "2023-06-22",
+            "title": "The Quest for Tom Sawyer's Gold",
+            "video": false,
+            "vote_average": 4.056,
+            "vote_count": 9
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/2vFuG6bWGyQUzYS9d69E5l85nIz.jpg",
+            "genre_ids": [
+                878,
+                12,
+                28
+            ],
+            "id": 667538,
+            "original_language": "en",
+            "original_title": "Transformers: Rise of the Beasts",
+            "overview": "When a new threat capable of destroying the entire planet emerges, Optimus Prime and the Autobots must team up with a powerful faction known as the Maximals. With the fate of humanity hanging in the balance, humans Noah and Elena will do whatever it takes to help the Transformers as they engage in the ultimate battle to save Earth.",
+            "popularity": 352.907,
+            "poster_path": "/gPbM0MK8CP8A174rmUwGsADNYKD.jpg",
+            "release_date": "2023-06-06",
+            "title": "Transformers: Rise of the Beasts",
+            "video": false,
+            "vote_average": 7.3,
+            "vote_count": 4481
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/lntyt4OVDbcxA1l7LtwITbrD3FI.jpg",
+            "genre_ids": [
+                18,
+                10749
+            ],
+            "id": 1010581,
+            "original_language": "es",
+            "original_title": "Culpa mía",
+            "overview": "Noah must leave her city, boyfriend, and friends to move into William Leister's mansion, the flashy and wealthy husband of her mother Rafaela. As a proud and independent 17 year old, Noah resists living in a mansion surrounded by luxury. However, it is there where she meets Nick, her new stepbrother, and the clash of their strong personalities becomes evident from the very beginning.",
+            "popularity": 289.377,
+            "poster_path": "/w46Vw536HwNnEzOa7J24YH9DPRS.jpg",
+            "release_date": "2023-06-08",
+            "title": "My Fault",
+            "video": false,
+            "vote_average": 7.936,
+            "vote_count": 2909
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/aGQIIKpAPYNtziFrCCBBgIzmUXe.jpg",
+            "genre_ids": [
+                27
+            ],
+            "id": 1051560,
+            "original_language": "en",
+            "original_title": "The Devil Comes at Night",
+            "overview": "A washed up boxer searching for his inheritance must fight for his life when he is trapped in his deceased father's farmhouse by a local cannibal cult.",
+            "popularity": 271.289,
+            "poster_path": "/6QIeZuKirT4cXVpW7ilVZmcRmdK.jpg",
+            "release_date": "2023-06-06",
+            "title": "The Devil Comes at Night",
+            "video": false,
+            "vote_average": 7.1,
+            "vote_count": 10
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/x2thJwMJ6oGlhn7UC4vSwHltEw0.jpg",
+            "genre_ids": [
+                28,
+                53,
+                10770
+            ],
+            "id": 1115396,
+            "original_language": "en",
+            "original_title": "Hunting Games",
+            "overview": "When a group of ex-military members is hired to retrieve a lost bag of stolen money, their mission becomes more difficult after a lone hunter finds the bag first.",
+            "popularity": 257.239,
+            "poster_path": "/xVbEJzdMxIQqpuLgla0hU8qr9mt.jpg",
+            "release_date": "2023-05-12",
+            "title": "Hunting Games",
+            "video": false,
+            "vote_average": 4.107,
+            "vote_count": 14
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/2KGxQFV9Wp1MshPBf8BuqWUgVAz.jpg",
+            "genre_ids": [
+                16,
+                28,
+                12,
+                35,
+                10751
+            ],
+            "id": 940551,
+            "original_language": "en",
+            "original_title": "Migration",
+            "overview": "After a migrating duck family alights on their pond with thrilling tales of far-flung places, the Mallard family embarks on a family road trip, from New England, to New York City, to tropical Jamaica.",
+            "popularity": 243.662,
+            "poster_path": "/ldfCF9RhR40mppkzmftxapaHeTo.jpg",
+            "release_date": "2023-12-06",
+            "title": "Migration",
+            "video": false,
+            "vote_average": 7.445,
+            "vote_count": 1710
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/kwzNUM4yZ26XuNAPSyaWwJeWRP4.jpg",
+            "genre_ids": [
+                28,
+                35,
+                14
+            ],
+            "id": 950526,
+            "original_language": "pt",
+            "original_title": "O Mestre da Fumaça",
+            "overview": "The journey of Gabriel and Daniel, two brothers cursed by the Chinese mafia with its feared Three Generations Revenge, who have already reaped the life of their grandfather and their father. To survive, one of the brothers must learn the Smoke Style secrets, a little known Cannabis martial art, taught by a single master, high up in the mountains.",
+            "popularity": 235.272,
+            "poster_path": "/mg6YkwftQOJjpT2ygYlCi11LWeC.jpg",
+            "release_date": "2023-05-18",
+            "title": "The Smoke Master",
+            "video": false,
+            "vote_average": 7.545,
+            "vote_count": 67
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/hU1Q9YVzdYhokr8a9gLywnSUMlN.jpg",
+            "genre_ids": [
+                16,
+                12,
+                28,
+                14,
+                10751
+            ],
+            "id": 1147400,
+            "original_language": "fr",
+            "original_title": "Miraculous World : Paris, Les Aventures de Toxinelle et Griffe Noire",
+            "overview": "Miraculous holders from another world appear in Paris. They come from a parallel universe where everything is reversed: the holders of Ladybug and Black Cat Miraculouses, Shadybug and Claw Noir, are the bad guys, and the holder of the Butterfly Miraculous, Hesperia, is a superhero. Ladybug and Cat Noir will have to help Hesperia counter the attacks of their evil doubles and prevent them from seizing the Butterfly's Miraculous. Can our heroes also help Hesperia make Shadybug and Claw Noir better people?",
+            "popularity": 234.911,
+            "poster_path": "/7Md3nuV0ZprBTnkdR3OrUCEsrSP.jpg",
+            "release_date": "2023-10-21",
+            "title": "Miraculous World: Paris, Tales of Shadybug and Claw Noir",
+            "video": false,
+            "vote_average": 7.241,
+            "vote_count": 430
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
+            "genre_ids": [
+                16,
+                10751,
+                12,
+                14,
+                35
+            ],
+            "id": 502356,
+            "original_language": "en",
+            "original_title": "The Super Mario Bros. Movie",
+            "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
+            "popularity": 224.113,
+            "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
+            "release_date": "2023-04-05",
+            "title": "The Super Mario Bros. Movie",
+            "video": false,
+            "vote_average": 7.655,
+            "vote_count": 8930
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/aswBReNN2adqTiOBnvh96RCDeJP.jpg",
+            "genre_ids": [
+                28,
+                80,
+                18
+            ],
+            "id": 959092,
+            "original_language": "fr",
+            "original_title": "Farang",
+            "overview": "Sam is a professional boxer about to get released from prison. While on parole, his past catches up with him and he has no choice but to flee. Five years later, he has rebuilt a simple life on an exotic island in Thailand with his wife and her daughter, but when he gets blackmailed by a dangerous local godfather, he must embark on a dangerous drug smuggling mission which results in a tragedy. Now he has only one purpose: to seek merciless vengeance.",
+            "popularity": 221.384,
+            "poster_path": "/u9035lysUz3ccloQt0SeIp1Mu8a.jpg",
+            "release_date": "2023-06-28",
+            "title": "Mayhem!",
+            "video": false,
+            "vote_average": 6.7,
+            "vote_count": 153
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/8AGwtYlvrZWEPo9zQthVd68zxNO.jpg",
+            "genre_ids": [
+                27
+            ],
+            "id": 1140168,
+            "original_language": "en",
+            "original_title": "Freddy's Fridays",
+            "overview": "A detective begins to investigate a series of mysterious murders that are connected to a demonic book that brings dolls to life. As the body count begins to rise, the detective soon learns the curse of the demonic Friday and must find a way to stop it before any others disappear.",
+            "popularity": 204.934,
+            "poster_path": "/8smsOtjp8FbBgL8YKrgCADJWEhe.jpg",
+            "release_date": "2023-10-13",
+            "title": "Freddy's Fridays",
+            "video": false,
+            "vote_average": 3.778,
+            "vote_count": 9
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/8wwXPG22aNMpPGuXnfm3galoxbI.jpg",
+            "genre_ids": [
+                28,
+                12,
+                10751,
+                35
+            ],
+            "id": 675353,
+            "original_language": "en",
+            "original_title": "Sonic the Hedgehog 2",
+            "overview": "After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. His test comes when Dr. Robotnik returns, this time with a new partner, Knuckles, in search for an emerald that has the power to destroy civilizations. Sonic teams up with his own sidekick, Tails, and together they embark on a globe-trotting journey to find the emerald before it falls into the wrong hands.",
+            "popularity": 199.063,
+            "poster_path": "/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg",
+            "release_date": "2022-03-30",
+            "title": "Sonic the Hedgehog 2",
+            "video": false,
+            "vote_average": 7.507,
+            "vote_count": 4898
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/4XM8DUTQb3lhLemJC51Jx4a2EuA.jpg",
+            "genre_ids": [
+                28,
+                80,
+                53
+            ],
+            "id": 385687,
+            "original_language": "en",
+            "original_title": "Fast X",
+            "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
+            "popularity": 193.194,
+            "poster_path": "/fiVW06jE7z9YnO4trhaMEdclSiC.jpg",
+            "release_date": "2023-05-17",
+            "title": "Fast X",
+            "video": false,
+            "vote_average": 7.093,
+            "vote_count": 5458
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/vIgyYkXkg6NC2whRbYjBD7eb3Er.jpg",
+            "genre_ids": [
+                878,
+                28,
+                12
+            ],
+            "id": 580489,
+            "original_language": "en",
+            "original_title": "Venom: Let There Be Carnage",
+            "overview": "After finding a host body in investigative reporter Eddie Brock, the alien symbiote must face a new enemy, Carnage, the alter ego of serial killer Cletus Kasady.",
+            "popularity": 192.14,
+            "poster_path": "/1MJNcPZy46hIy2CmSqOeru0yr5C.jpg",
+            "release_date": "2021-09-30",
+            "title": "Venom: Let There Be Carnage",
+            "video": false,
+            "vote_average": 6.796,
+            "vote_count": 9919
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/jr8tSoJGj33XLgFBy6lmZhpGQNu.jpg",
+            "genre_ids": [
+                16,
+                12,
+                14,
+                35,
+                10751
+            ],
+            "id": 315162,
+            "original_language": "en",
+            "original_title": "Puss in Boots: The Last Wish",
+            "overview": "Puss in Boots discovers that his passion for adventure has taken its toll: He has burned through eight of his nine lives, leaving him with only one life left. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives.",
+            "popularity": 190.502,
+            "poster_path": "/kuf6dutpsT0vSVehic3EZIqkOBt.jpg",
+            "release_date": "2022-12-07",
+            "title": "Puss in Boots: The Last Wish",
+            "video": false,
+            "vote_average": 8.227,
+            "vote_count": 7612
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/f7Qwg7kKNIvBnr3Ipv2bpXb23BN.jpg",
+            "genre_ids": [
+                27,
+                9648,
+                878,
+                53
+            ],
+            "id": 1024721,
+            "original_language": "en",
+            "original_title": "Monolith",
+            "overview": "A desperate young journalist turns to podcasting to salvage her career, but her rush to make headlines leads her to an alien conspiracy.",
+            "popularity": 182.859,
+            "poster_path": "/zKpLzzX1va6gkMJiI9p3DudqChe.jpg",
+            "release_date": "2023-10-26",
+            "title": "Monolith",
+            "video": false,
+            "vote_average": 5.928,
+            "vote_count": 90
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/9Y9wTgrMk4G1YNPDDekNk7dybwx.jpg",
+            "genre_ids": [
+                28,
+                12,
+                10752
+            ],
+            "id": 1152624,
+            "original_language": "en",
+            "original_title": "3 Days in Malay",
+            "overview": "Marines stationed at an airfield in Malay during WWII get wind of a coming raid by the Japanese. Unable to get reinforcements approved, they engage in a harrowing 3-day battle against enemy forces.",
+            "popularity": 176.468,
+            "poster_path": "/hqnfqeCILvgKGWKOut5lVdxdeJh.jpg",
+            "release_date": "2023-08-11",
+            "title": "3 Days in Malay",
+            "video": false,
+            "vote_average": 5.074,
+            "vote_count": 27
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/zBG5Mg29NH9xxpWMMG7BIvKwYhL.jpg",
+            "genre_ids": [
+                10749,
+                18
+            ],
+            "id": 829557,
+            "original_language": "pl",
+            "original_title": "365 dni: Ten dzień",
+            "overview": "Laura and Massimo are back and hotter than ever. But the reunited couple's new beginning is complicated by Massimo’s family ties and a mysterious man who enters Laura’s life to win her heart and trust, at any cost.",
+            "popularity": 168.572,
+            "poster_path": "/7qU0SOVcQ8BTJLodcAlulUAG16C.jpg",
+            "release_date": "2022-04-26",
+            "title": "365 Days: This Day",
+            "video": false,
+            "vote_average": 5.882,
+            "vote_count": 1482
+        }
+    ],
+    "total_pages": 7644,
+    "total_results": 152878
+}
